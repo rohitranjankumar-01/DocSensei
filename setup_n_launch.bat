d1b9cc2b4cc3d4bbd508431c6dc2ba7b16675fa8
Rohit Ranjan Kumar <rohitkashyap3009@gmail.com>
2026-07-09 15:25:03 +0530

    Adding setup_and_launch.bat for easier startup of DocSensei and adding leftovers

---
+++ setup_n_launch.bat
@@ -0,0 +1,15 @@
+@echo off
+echo [1/4] Creating virtual environment with Python 3.12...
+:: Force the use of Python 3.12 specifically
+py -3.11 -m venv venv
+call venv\Scripts\activate
+
+echo [2/4] Upgrading tools to ensure we get binary wheels...
+python -m pip install --upgrade pip setuptools wheel
+
+echo [3/4] Installing dependencies...
+pip install -r requirements.txt
+
+echo [4/4] Launching DocSensei...
+streamlit run app.py
+pause
